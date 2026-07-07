ad0e5cf05a8c0aea46b85c114846304a6bf163a8
Eric <ericbency@gmail.com>
2026-07-07 20:31:25 +0200

    completed employee form

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ const add_employee_form = document.getElementById("add_employee_form");
 employee_add_btn.addEventListener("click", function() {
     add_employee_form.style.display = "flex";
 })
+
+const employee_close_btn = document.getElementById("cancel-btn");
+employee_close_btn.addEventListener("click", function(){
+    add_employee_form.style.display = "none";
+})
